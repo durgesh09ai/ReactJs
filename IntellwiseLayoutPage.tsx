fcfec09a001e9cc7ae5820749d98d6885db26d06
Durgesh kumar <durgesh09ai@gmail.com>
2025-06-26 18:43:07 +0530

    Delete IntellwiseLayoutPage.tsx

--- IntellwiseLayoutPage.tsx
+++
@@ -1,7 +0,0 @@
-import { IntellwiseLayout } from "../components/IntellwiseLayout/IntellwiseLayout";
-
-function IntellwiseLayoutPage() {
-  return <IntellwiseLayout />;
-}
-
-export default IntellwiseLayoutPage;
